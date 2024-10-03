1f7b09ccb9571961de5e85a9bd6b34dc53df7a4a
Vitor Assis <vitorassis997@gmail.com>
2024-10-03 17:11:41 -0300

    html, css e js quase finalizados

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getApi () {
     const ul = document.querySelector('ul');
     const user = document.querySelector('#user').value; /*.value faz com que pegamos o valor que será escrito dentro desse espaço, no caso, o nome no perfil*/
     ul.innerHTML = ''/*garantimos que a ul vai estar vazia para ser preenchida, apartir do momento do click, evitando que ao clicar duas vezes ou mais, ele fique repitindo a pesquisa e concatenando tudo na coluna*/ 
-
+    
     fetch(`https://api.github.com/users/${user}/repos`)
        /*res = resposta*/
       .then(async res => { 
@@ -16,8 +16,56 @@ function getApi () {
 
          let hr = document.createElement('hr');
          
-         li.innerHTML = ` <strong> ${item.name} </strong>
-         <span>${item.html_url}</span> `
+         let circleColor;
+         if(item.language) {
+            switch (item.language) {
+                case 'HTML' :
+                    circleColor = 'orange';
+                    break;
+                case 'CSS' :
+                    circleColor = 'purple';
+                    break;
+                case 'JavaScript' :
+                    circleColor = 'yellow';
+                    break;
+                case 'C++' :
+                    circleColor = 'pink';
+                    break;
+                case 'Python' :
+                    circleColor = 'green';
+                    break;
+                case 'Java' :
+                    circleColor = 'blue';
+                    break;
+                case 'C' :
+                    circleColor = 'gray';
+                    break;
+                default :
+                    circleColor = 'black';
+            } 
+        } else {
+            circleColor = 'white';
+        }
+
+         li.innerHTML = ` 
+        <div class="container">
+            <div class="name-date">
+                <h4 class="rep_name"> 
+                    <a href="${item.html_url}">${item.name}</a>
+                </h4>
+                <span class="data-create">${new Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</span>
+            </div>
+            <div>
+                <span style="color:gray">${item.html_url}</span> 
+                <span class="language">
+                    <span class="circle" style="background-color:${circleColor}">
+                    </span>${item.language}
+                </span>
+            </div> 
+        </div>
+        `
+
+         
 
          ul.appendChild(li);
          ul.appendChild(hr);
