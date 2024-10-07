deb88700f1f3d043599acc69ecb8717fecd2856b
Vitor Assis <vitorassis997@gmail.com>
2024-10-07 16:53:22 -0300

    arrumando ultimos detalhes no css

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ function getApi () {
     const ul = document.querySelector('ul');
     const user = document.querySelector('#user').value; /*.value faz com que pegamos o valor que será escrito dentro desse espaço, no caso, o nome no perfil*/
     ul.innerHTML = ''/*garantimos que a ul vai estar vazia para ser preenchida, apartir do momento do click, evitando que ao clicar duas vezes ou mais, ele fique repitindo a pesquisa e concatenando tudo na coluna*/ 
-    
+    const pesquisa = document.querySelector('.pesquisa');
+    const hr_rep = document.querySelector('.hr_rep');
+
     fetch(`https://api.github.com/users/${user}/repos`)
        /*res = resposta*/
       .then(async res => { 
 
         let data = await res.json();
+        pesquisa.classList.add('exibir');
+        hr_rep.classList.add('exibir_hr');
 
         data.map(item => { /*propriedade usada por ser uma array*/
 
          let li = document.createElement('li');
           li.classList.add('m-10');
-
-         let hr = document.createElement('hr');
+         let hr = document.createElement('hr');     
          
          let circleColor;
          if(item.language) {
@@ -55,7 +58,7 @@ function getApi () {
                 </h4>
                 <span class="data-create">${new Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</span>
             </div>
-            <div>
+            <div class="url_language">
                 <span style="color:gray">${item.html_url}</span> 
                 <span class="language">
                     <span class="circle" style="background-color:${circleColor}">
@@ -64,9 +67,6 @@ function getApi () {
             </div> 
         </div>
         `
-
-         
-
          ul.appendChild(li);
          ul.appendChild(hr);
 
@@ -81,8 +81,9 @@ function getApi () {
           console.log(e);
           ul.innerHTML = '';
           let li = document.createElement('li');
-          li.classList.add('m-10');
-          li.innerHTML = ` <strong>Erro ao buscar usuário. Tente novamente!</strong>`
+          li.innerHTML = ` 
+          <strong class="msg_erro">Erro ao buscar usuário. Tente novamente!</strong>
+          `
           ul.appendChild(li);
        })
 
